e4e284b50f764c8d75a1fc5b98674aa9a712ea11
Sean Hoessmann <Sean@Seans-MBP.hitronhub.home>
2017-07-20 19:27:54 -0700

    Added some simple tests and it hopefully works now.

--- Client_database.py
+++ Client_database.py
@@ -5,31 +5,67 @@ class client_database:
 
     chatrooms = []
 
-    def add_chatroom(room):
+    def add_chatroom(self,room):
         if room in self.chatrooms:
             return False
         self.chatrooms.append(room)
         return True
 
-    def remove_chatroom(room):
+    def remove_chatroom(self,room):
         if room not in self.chatrooms:
             return False
+        self.chatrooms.remove(room)
         return True
 
-    def list_chatrooms():
+    def list_chatrooms(self):
         return self.chatrooms[:]
 
-    def add_message(message,room):
+    def add_message(self,message,room):
         if room not in self.chatrooms:
             return False
         i = self.chatrooms.index(room)
         self.chatrooms[i].add_message(message)
+        return True
 
-    def load_from_chatroom(room):
+    def load_from_chatroom(self,room):
         if room not in self.chatrooms:
             return False
         i = self.chatrooms.index(room)
         return self.chatrooms[i].get_history()
 
 if __name__ == "__main__":
-    # tests
+    print ""
+    cd = client_database()
+
+    print "Listing chatrooms:"
+    print cd.list_chatrooms()
+    print ""
+
+    print "Adding message to room that's not in the list:"
+    room = chatroom("general")
+    print cd.add_message("testing...?",room)
+    print ""
+
+    print "Adding new chatroom called 'general'"
+    print cd.add_chatroom(room)
+    print ""
+
+    print "Adding 'hello' as a message to general"
+    print cd.add_message("hello",room)
+    print ""
+
+    print "Listing chatrooms"
+    print cd.list_chatrooms()
+    print ""
+
+    print "Printing history"
+    print cd.load_from_chatroom(room)
+    print ""
+
+    print "Removing general"
+    print cd.remove_chatroom(room)
+    print ""
+
+    print "Listing chatrooms"
+    print cd.list_chatrooms()
+    print ""
