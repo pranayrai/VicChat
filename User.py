1e4f386947d8652a20e45a9854d021875f35f3b2
pranayrai <pranay1995@gmail.com>
2017-07-22 13:10:29 -0700

    Updating User class

--- User.py
+++ User.py
@@ -1,37 +1,54 @@
 from Chatroom import chatroom
 
-class User:
-
-	username = ''
-	userStatus = None
-	guestUser = None	
-	chatrooms = []
+class user():	
 	
-	def __init__(self, username, userStatus):
+	
+	def __init__(self, username):
 		self.username = username
-		self.userStatus = userStatus
+		self.userStatus = None
+		self.guestUser = None
+		self.userList = []
 		
 	def get_username(self):
-		return username
+		return self.username
 	
 	def get_user_status(self, user):
-		if user in self.chatrooms:
-			return True
+		if user in self.userList:
+			return "Active"
+		else: 
+			self.guestUser = True
+			return "Inactive"
 	
 	def get_chat_rooms(self):
-		return self.chatrooms[:]
+		return self.chatroom[:]
 		
-	def add_chatroom(self,room):
-            if room in self.chatrooms:
+	def add_user(self,user):
+            if user in self.userList:
                 return False
-            self.chatrooms.append(room)
+            self.userList.append(user)
             return True
 		
-	def leave_chatroom(self,room):
-            if room not in self.chatrooms:
+	def leave_chatroom(self,user):
+            if user not in self.userList:
                 return False
-            self.chatrooms.leave(room)
+            self.userList.remove(user)
             return True
+			
+if __name__ == "__main__":
+	print ""
+	u = user("BOB")
+	
+	
+	u.userList.append("BOB")
+	print u.userList
+	
+	print u.get_username()
+	
+	print "User Status:" + u.get_user_status("BOB")
+	
+	
+	
+	
 		
 	
 		
