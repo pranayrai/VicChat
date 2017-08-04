f9639cd9a914dd989d95df76520e267cca55a9c9
Sean Hoessmann <seanohoessmann@gmail.com>
2017-08-04 11:41:22 -0700

    chatroom names cannot contain spaces
    why does git think i edited all the whitespace?

--- GUI.py
+++ GUI.py
@@ -19,96 +19,100 @@ class GUIWindow(QWidget):
 
 	def __init__(self, parent=None):
 		QWidget.__init__(self, parent)
-		
+
 		self.username = ''
 		self.currentRoom = 'general'
-		
+
 		# Name and resize the main window
 		self.setWindowTitle('VicChat GUI prototype')
 		self.resize(320, 310)
-		
+
 		# Create the output button
 		self.btn = QPushButton('Print text to console', self)
-		
+
 		# Create textbox for data-entry
 		self.inputBox = QLineEdit(self)
 		self.inputBox.setPlaceholderText("enter message here")
-		
+
 		# Create a multi-line textbox to display history
 		self.outputBox = QTextEdit(self)
 		self.outputBox.setReadOnly(True)
-		
+
 		# Add the room label
 		self.roomLabel = QLabel(self)
 		self.roomLabel.setText("Room: ")
-		
+
 		# Create combobox for room list
 		self.rooms = QComboBox(self)
-		
+
 		# Button to add a new room
 		self.addRoomBtn = QPushButton('New Room', self)
 		self.addRoomBtn.setToolTip('Click to create a new room')
-		
+
 		self.roomListBtn = QPushButton('Room List', self)
 		self.roomListBtn.setToolTip('Click to show list of rooms')
-		
+
 		# Place everything on the screen using a layout
 		self.layout = QVBoxLayout()
 		self.topL = QHBoxLayout()
 		self.topL.addWidget(self.roomLabel)
 		self.topL.addWidget(self.rooms)
-		
+
 		self.topL.addWidget(self.roomListBtn)
 		self.topL.addStretch()
 		self.topL.addWidget(self.addRoomBtn)
-		
+
 		self.layout.addLayout(self.topL)
 		self.layout.addWidget(self.outputBox)
 		self.layout.addWidget(self.inputBox)
 		self.layout.addWidget(self.btn)
-		
+
 		self.setLayout(self.layout)
-		
+
 		# self.currentRoom = self.rooms.currentText()
-		
+
 		# SET UP THE CLIENT IN A NEW THREAD:
 		self.client = client_networking()
 		self.thread = QThread(self)
 		self.client.messageSignal.connect(self.receive_info)
-		
+
 		self.client.roomListSignal.connect(self.room_list)
-		
+
 		self.client.errorSignal.connect(self.error_output)
 		self.client.greenSignal.connect(self.green_output)
 		self.client.joinRoomSignal.connect(self.join_room)
 		self.client.delRoomSignal.connect(self.delete_room)
-		
+
 		self.client.moveToThread(self.thread)
 		self.thread.started.connect(self.client.run)
-		
+
 		# Connect the buttons/input to client:
 		self.inputBox.returnPressed.connect(self.on_press_print)
 		self.btn.pressed.connect(self.on_press_print)
 		self.rooms.currentIndexChanged.connect(self.change_room)
 		self.addRoomBtn.clicked.connect(self.new_room)
-		
+
 		self.roomListBtn.clicked.connect(lambda: self.client.get_room_list())
-		
+
 		# Start the thread!
 		self.thread.start()
-		
+
 	# This is called when the new room button is pressed
 	@pyqtSlot()
 	def new_room(self):
 		text, ok = QInputDialog.getText(self, 'Create new room', 'Enter a room name:')
 		if ok:
-			self.rooms.addItem(text)
-			self.rooms.setCurrentIndex(self.rooms.count() - 1)
-			self.client.create_room(text)
-			#self.client.join_room(self.rooms.currentText())
-			self.currentRoom = self.rooms.currentText()
-			# self.commands.append("New room: " + text)
-	
+			if ' ' in text:
+				self.error_output("Chatroom names cannot contain spaces.")
+				return
+			else:
+				self.rooms.addItem(text)
+				self.rooms.setCurrentIndex(self.rooms.count() - 1)
+				self.client.create_room(text)
+				#self.client.join_room(self.rooms.currentText())
+				self.currentRoom = self.rooms.currentText()
+				# self.commands.append("New room: " + text)
+
 	# This is called when the room selection is changed
 	@pyqtSlot()
 	def change_room(self):
@@ -122,40 +126,40 @@ class GUIWindow(QWidget):
 		if hist is not False:
 			for i in hist:
 				self.outputBox.append(i)
-	
+
 	# Sends the message to the server, then clears the inputBox.
 	@pyqtSlot()
 	def on_press_print(self):
 		self.client.send_message(self.inputBox.text())
 		self.inputBox.clear()
-	
+
 	def change_windows(self):
 		self.login.hide()
 		# self.app.setStyle(QStyleFactory.create(themeList.currentText()))
 		self.username = self.nameInput.text()
 		self.show()
-	
+
 	# Receive messages from the server and display them to chat.
 	@pyqtSlot(str)
 	def receive_info(self, msg):
 		self.outputBox.append(msg)
-	
+
 	# Receive messages from the server and display them to chat.
 	@pyqtSlot(str)
 	def green_output(self, msg):
 		self.outputBox.append("<span style=\" color:#008000;\" >{}</span>".format(msg))
-	
+
 	@pyqtSlot(str)
 	def error_output(self, msg):
 		self.outputBox.append("<span style=\" color:#ff0000;\" >{}</span>".format(msg))
-	
+
 	@pyqtSlot(str)
 	def get_new_room(self, roomList):
 		roomList = roomList.split(' ')
 		for i in roomList:
 			# if i != "general":
 			self.rooms.addItem(i)
-	
+
 	@pyqtSlot(str)
 	def join_room(self, room):
 		joinedRooms = [self.rooms.itemText(i) for i in range(self.rooms.count())]
@@ -166,21 +170,21 @@ class GUIWindow(QWidget):
 			self.nd.hide()
 		except:
 			return
-	
+
 	@pyqtSlot(str)
 	def delete_room(self, room):
 		self.rooms.removeItem(self.rooms.findText(room))
 		self.nd.hide()
-	
+
 	@pyqtSlot(str)
 	def room_list(self, roomList):
-		
+
 		self.nd = RoomListWindow()
 		self.rlayout = QVBoxLayout()
 		roomList = roomList.split("\n")[1:]
 		# roomList = roomList[1:]
 		joinedRooms = [self.rooms.itemText(i) for i in range(self.rooms.count())]
-		
+
 		buttons = []
 		for i in roomList:
 			tempLayout = QHBoxLayout()
@@ -201,14 +205,14 @@ class GUIWindow(QWidget):
 			self.rlayout.addWidget(button)'''
 		self.nd.setLayout(self.rlayout)
 		self.nd.show()
-	
+
 	def select_room(self, data="\n"):
 		self.client.change_room(data)
 		print data
 		print "i join this room!"
-	
+
 	# self.roomlistwindow.hide()
-	
+
 	def leave_room(self, data="\n"):
 		self.client.leave_room(data)
 		print "leaving room"
