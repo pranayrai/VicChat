ce947cab8605aeef84be51c08693acea24eb7309
Gregory O'Hagan <gregoryrohagan@gmail.com>
2017-08-04 13:18:16 -0700

    added some comments in server

--- Server_networking.py
+++ Server_networking.py
@@ -3,17 +3,21 @@ import sys
 import threading
 import time
 from Server_data import server_data
+from datetime import datetime
 
 s = socket.socket()
 host = socket.gethostname()
 port = 9999
 s.bind((host, port))
 
+# clients will store a list of tuples (socket, username) corresponding to all active users
 clients = []
+# data holds all chat room and user objects
 data = server_data()
+# Initialize a general chatroom. This will be the default chatroom
 data.add_chatroom('general')
 
-
+# Continuously listen for input from clients
 def receive():
 	while True:
 		q = None
@@ -32,7 +36,7 @@ def receive():
 				print "User disconnected: " + cli[1]
 				clients.remove(cli)
 
-
+# Continuously listen for new clients
 def listen_connections():
 	s.listen(5)
 	print('[Waiting for connections...]')
@@ -44,7 +48,8 @@ def listen_connections():
 threading.Thread(target = receive).start()
 threading.Thread(target = listen_connections).start()
 
-
+# Once a new client connects, this method is started in a new thread
+# This prevents one user from preventing other users from joining by stalling here
 def get_username(c):
 	c.send("/sysmessage Enter a username")
 	try:
@@ -52,17 +57,22 @@ def get_username(c):
 			msg = c.recv(1024)
 			y = msg.split()
 			if len(y) < 3:
+				# If this block runs, the client_networking is sending poorly
+				# constructed messages
 				c.send("/error Incorrect message format. Please update the client")
 				continue
 			name = str(y[2])
 			rooms = data.add_user(name, c)
 			if name and rooms:
+				#If there is a list of rooms, the user is a permanent user
+				#Send them the history of all of these rooms.
 				c.settimeout(0.001)
 				clients.append((c, name))
 				if type(rooms) is list:
 					for r in rooms:
 						c.send("/history " + r + " " + data.chatroom_history(r))
 				else:
+					# If the user is a guest, put them in the general chatroom
 					data.link_user_chatroom(name, 'general')
 					c.send("/history general " + data.chatroom_history('general'))
 				break
@@ -71,7 +81,8 @@ def get_username(c):
 	except socket.error:
 		pass
 
-
+# Send a message to all clients in a list
+# li is a list of tuples: (socket, username)
 def update_clients(li, msg):
 	for item in li:
 		try:
@@ -80,7 +91,7 @@ def update_clients(li, msg):
 			data.remove_user(item[1])
 			print "User disconnected: " + item[1]
 
-
+#Process user input, and send it where required (database, update clients, etc...)
 def process_message(c, username, msg):
 	"""
 	- check if the string starts with a /
@@ -108,13 +119,19 @@ def process_message(c, username, msg):
 			c.send("/error Incorrect input format")
 			return
 		if cmd == "/addmessage":
-			messageText = username + ": " + text
+			#Timestamp the message, then add the username who sent it
+			time = str(datetime.now())
+			time = time[11:16]
+			messageText = time + " " + username + ": " + text
 			li = data.add_message(messageText, arg)
 			if li:
+				# Distribute the message to all connected clients
 				update_clients(li, arg + " " + messageText)
 			else:
+				#If data.add_message doesn't return a list, the chat room doesn't exist
 				c.send("/error The chat room does not exist")
 		elif cmd == "/joinchatroom":
+			#Link the user to a chatroom, and send them that chatroom's history
 			data.link_user_chatroom(username, arg)
 			hist = data.chatroom_history(arg)
 			if hist:
